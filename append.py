29a6fbf22fddbbccc86beda982c67ff20941b995
shubh <shubhsurvat@gmail.com>
2024-03-11 19:29:15 +0000

    commited append program using python

---
+++ append.py
@@ -0,0 +1,10 @@
+# adding single item in list
+a = [10,20,30,40]
+a.append(50)
+print(a)
+# adding multiple item in list
+bastman=[]
+for b in range(5):
+    name = input('enter a bastman name :')
+    bastman.append(name)
+print(bastman)
